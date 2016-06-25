8d5e5f21d4e74e4dd5ba6e54a96f52e53491c96e
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-06-25 17:27:47 +0900

    Add codes

---
+++ index.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+const arch = { x64: "amd64", ia32: "386", arm: "arm" }[process.arch];
+const os = { win32: "windows", linux: "linux", darwin: "darwin"}[process.platform];
+const ext = { win32: ".exe" }[process.platform] || "";
+const binName = `apex_${os}_${arch}${ext}`;
+const binPath = path.join(__dirname, binName);
+
+module.exports = {
+  download,
+  run
+};
+
+function run(args, tryDownload = true) {
+  return new Promise((ok, ng) => {
+    require('child_process')
+      .spawn(binPath, args, {stdio: 'inherit'})
+      .on('exit', ok)
+      .on('error', ng)
+  })
+  .then(process.exit)
+  .catch(err => {
+    if(tryDownload && err.code == 'ENOENT') {
+      return download().then(() => run(args, false))
+    }
+    console.error(err);
+    process.exit(1)
+  })
+}
+
+function download() {
+  const fetch = require('node-fetch');
+  const binPathTmp = `${binPath}.tmp`;
+  return fetch("https://api.github.com/repos/apex/apex/releases")
+    .then(res => res.json())
+    .then(json => fetch(json[0].assets.filter(a => a.name == binName)[0].browser_download_url))
+    .then(res => new Promise((ok, ng)=>{
+      const w = res.body.pipe(fs.createWriteStream(binPathTmp, {mode: 0755}));
+      w.once('error', ng);
+      w.once('finish', ok);
+    }))
+  .then(() => new Promise((ok, ng)=>{
+    fs.rename(binPathTmp, binPath, (err) => {
+      if(err) return ng(err);
+      ok()
+    })
+  }))
+  .catch(err => new Promise((ok, ng) => {
+    fs.unlink(binPathTmp, (err) => {
+      if(err) return ng(err);
+      ok()
+    })
+  }))
+}
